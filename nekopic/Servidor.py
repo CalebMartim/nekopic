344e87a84e8973e01f5b85a1495a0ee8d775a54c
Guilherme da Rocha <109252470+GRochaC@users.noreply.github.com>
2023-11-04 10:28:59 -0300

    criacao da pasta nekopic

---
+++ nekopic/Servidor.py
@@ -0,0 +1,174 @@
+# bibliotecas essenciais
+import ftplib
+import os
+
+class Servidor:
+    def __init__(self,host,user,password, wd = None):
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__wd = wd
+
+        # inicia o servidor com um timeout de 30s,
+        # isto e, ha um tempo de espera de no maximo
+        # 30s para se conectar no servidor antes de ser removido
+        self.__ftp = ftplib.FTP_TLS(timeout = 30)
+
+    # retorna o objeto FTP
+    def get_ftp(self):
+        return self.__ftp
+    
+    # retorna uma lista dos arquivos presentes no diretorio de trabalho atual do servidor
+    def get_files(self):
+        return self.__ftp.nlst()
+
+    # conecta no servidor dado host
+    def connect(self):
+        self.__ftp.connect(host=self.__host)
+
+    # login no servidor dado user e senha
+    def login(self):
+        self.__ftp.login(user=self.__user,passwd=self.__password)
+
+    # estabelece um conexao segura (servidor FTP_TLS)
+    def secure_data_connection(self):
+        self.__ftp.prot_p()
+
+    # sai do servidor
+    def quit(self):
+        self.__ftp.quit()
+
+    # define um novo diretorio de trabalho atual
+    def set_wd(self, new_wd):
+        try:
+            if new_wd == self.__wd: return
+
+            self.__wd = new_wd
+            self.__ftp.cwd(self.__wd)
+
+        except ftplib.error_perm as e:
+            print(f"Erro: {e}")
+            return
+    
+    # retorna o nome do arquivo dependendo do OS
+    def __get_arquivo(arquivo):
+        # linux
+        if "/" in arquivo:
+            return arquivo.split("/")[-1]
+        
+        # windows
+        else:
+            return arquivo.split("\\")[-1]
+    
+    # retorna o diretorio dependendo do OS
+    def __get_diretorio(diretorio):
+        # linux
+        if "/" in diretorio:
+            return diretorio + "/"
+        
+        # windows
+        else:
+            return diretorio + "\\"
+
+    # Upload de imagem no servidor
+    def upload_image(self, arquivo):
+        # define o diretorio de trabalho onde sera feito o upload
+        self.set_wd(self.__wd)
+
+        try:
+            try:
+                # abre o arquivo
+                with open(arquivo, "rb") as file:
+                    arquivo_upload = Servidor.__get_arquivo(arquivo)
+
+                    # upload
+                    ret = self.__ftp.storbinary(f'STOR {arquivo_upload}', file, blocksize = 1024*1024)
+                    
+                    # fecha o arquivo
+                    file.close()
+                
+                # mensagem de retorno: 226 = sucesso
+                return ret.startswith("226")
+            
+            except FileNotFoundError:
+                print(f"\"{arquivo}\" nao encontrado")
+                return False
+        
+        except ftplib.error_perm as e:
+            print(f"Erro: {e}")
+            return False
+    
+    # Download de imagem no servidor
+    def download_image(self, arquivo, diretorio_destino):
+        # define o diretorio de trabalho onde sera feito o download
+        self.set_wd(self.__wd)
+
+        try:
+            # verifica se o diretorio de destino existe na maquina do usuario
+            if not os.path.exists(diretorio_destino):
+                print("Diretorio inexistente.")
+                return False
+
+            # verifica se o arquivo se encontra no diretorio de trabalho atual
+            if arquivo not in self.get_files():
+                print(f"\"{arquivo}\" inexistente.")
+                return False
+            
+            destino = Servidor.__get_diretorio(diretorio_destino)
+
+            # abre o arquivo
+            with open(f"{destino}{arquivo}", "wb") as file:
+                # download
+                ret = self.__ftp.retrbinary(f"RETR {arquivo}", file.write)
+
+                # fecha o arquivo
+                file.close()
+            
+            # mensagem de retorno: 226 = sucesso
+            return ret.startswith("226")
+            
+        except ftplib.error_perm as e:
+            print(f"Erro: {e}")
+            return False
+    
+    # Deletar uma imagem do servidor
+    def delete_image(self, arquivo):
+        # define o diretorio de trabalho onde se deletara um arquivo 
+        self.set_wd(self.__wd)
+ 
+        try:
+            # verifica se o arquivo se encontra no diretorio de trabalho atual
+            if arquivo not in self.get_files():
+                print(f"\"{arquivo}\" nao encontrado.")
+                return False
+            
+            # deleta o arquivo
+            ret = self.__ftp.delete(arquivo)
+
+            # mensagem de retorno: 250 = sucesso
+            return ret.startswith("250")
+
+        except ftplib.error_perm as e:
+            print(f"Erro {e}")
+            return False
+        
+    # Renomear uma imagem no servidor
+    def rename_file(self, arquivo, novo_nome):
+        # define o diretorio de trabalho onde se alterara o nome de uma imagem
+        self.set_wd(self.__wd)
+
+        try:
+            # verifica se o arquivo se encontra no diretorio de trabalho atual
+            if arquivo not in self.__ftp.nlst():
+                print(f"\"{arquivo}\" nao encontrado.")
+                return False
+            
+            # renomeia o arquivo
+            ret = self.__ftp.rename(arquivo, novo_nome)
+
+            # mensagem de retorno: 250 = sucesso
+            return ret.startswith("250")
+        
+        except ftplib.error_perm as e:
+            print(f"Erro: {e}")
+            return False
---
+++ nekopic/main.py
@@ -0,0 +1,98 @@
+# importa a classe Servidor
+from nekopic.Servidor import Servidor
+
+# funcao de introducao do programa: apresenta as operacoes do servidor
+def introducao():
+    ret = f"Digite:\n\t1.Upload\n\t2.Download\n\t3.Deletar\n\t4.Renomear arquivo\n\t0.Sair\n"
+    return ret
+
+# entrar no servidor
+servidor = Servidor("192.168.68.122","servidor_redes","servidorredes123")
+servidor.connect()
+servidor.login()
+servidor.set_wd("/files/images")
+
+# conexao TLS
+servidor.secure_data_connection()
+
+# se conseguimos conectar ao servidor, uma mensagem de bem-vindo é gerada
+if servidor.get_ftp().getwelcome().startswith("220"): 
+    print("Bem-vindo!")
+
+    # loop principal do programa
+    while True:
+        try:
+            comando = int(input(introducao()))
+
+            # Upload de imagens
+            if comando == 1:
+                # path de onde se encotra(m) o(s) arquivo(s) para ser(em) enviado(s) ao servidor
+                arquivos = input("Digite o caminho do(s) arquivo(s): ").split()
+
+                # para cada arquino na lista de arquivos para upload
+                for arquivo in arquivos:
+                    if servidor.upload_image(arquivo):
+                        print("Upload com sucesso")
+
+                    else:
+                        print("Upload fracassado")
+
+            # Download de imagens
+            elif comando == 2:
+                # nome do(s) arquivo(s) no servidor
+                arquivos = input("Digite o(s) nome(s) do(s) arquivo(s): ").split()
+
+                # onde sera armazenado o(s) arquivo(s) desejado(s)
+                destino = input("Digite o caminho de destino do(s) arquivo(s): ")
+
+                # para cada arquivo na lista de arquivos para download
+                for arquivo in arquivos:
+                    if servidor.download_image(arquivo, destino):
+                        print("Download com sucesso")
+                    
+                    else:
+                        print("Download fracassado")
+
+            # Deletar arquivos do servidor
+            elif comando == 3:
+                # nome do arquivo a ser deletado do servidor
+                arquivo = input("Digite o nome do arquivo a ser deletado do servidor: ")
+                
+                if servidor.delete_image(arquivo):
+                    print("Arquivo deletado com sucesso")
+                
+                else:
+                    print("Nao foi possivel deletar o arquivo")
+            
+            # Renomear um arquivo do servidor
+            elif comando == 4:
+                # nome do arquivo no servidor a ser renomeado
+                arquivo = input("Digite o nome do arquivo que voce deseja renomear: ")
+
+                # novo nome do arquivo desejado
+                novo_nome = input("Digite o novo nome: ")
+
+                if servidor.rename_file(arquivo, novo_nome):
+                    print("Nome alterado com sucesso")
+
+                else:
+                    print("Nao foi possivel alterar o nome do arquivo")
+
+            # sair do loop 
+            elif comando == 0: 
+                break
+
+            # quebra de linha da proxima exibicao da mensagem de introducao
+            print()
+        
+        # caso seja fornecido outro tipo de entrada
+        except ValueError:
+            print("Comando inválido\n")
+
+    # Sair do servidor
+    servidor.quit()
+
+
+# caso contrario
+else:
+    print("Nao foi possivel entrar no servidor.")
